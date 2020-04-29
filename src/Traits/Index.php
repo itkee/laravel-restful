ac121447c67d630c74c37350db5ccef9c924ef9a
SuperMan <itkee@vip.qq.com>
2020-04-29 16:33:47 +0800

    update

--- src/Traits/Index.php
+++ src/Traits/Index.php
@@ -17,8 +17,7 @@ trait Index
     public function index(Request $request)
     {
         //构造查询条件
-        $where = static::beforeIndex($request);
-
+        $where = static::beforeIndex($request)?static::beforeIndex($request):[];
         $model = static::getModelFQCN();
         $resource = static::getResourceFQCN();
         try{
