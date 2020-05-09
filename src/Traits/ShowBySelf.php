57f32fe4c560cc57dd23680f6e39d5e7e1676e80
SuperMan <itkee@vip.qq.com>
2020-05-09 15:07:57 +0800

    update

---
+++ src/Traits/ShowBySelf.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Itkee\LaravelRestful\Traits;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+trait ShowBySelf
+{
+
+    /**
+     * @param Request $request
+     * 查询前操作
+     */
+    protected function beforeShow(Request $request){}
+
+    /**
+     * Show
+     *
+     * @return JsonResource
+     *
+     * @author hanmeimei
+     */
+    public function show(Request $request)
+    {
+        $model = static::getModelFQCN();
+        $resource = static::getResourceFQCN();
+        //保存操作前进行数据验证
+        $beforeResult = $this->beforeShow($request);
+        if(is_array($beforeResult) && $beforeResult['statusCode'] !== 200){
+            return response()->json(['error' => $beforeResult['error']], $beforeResult['statusCode']);
+        };
+        
+        if($resource){
+            return new $resource($model::withTrashed()->find($request->get('uid')));
+        }else{
+            return response()->json($model::withTrashed()->find($request->get('uid')), 200);
+        }
+    }
+}
